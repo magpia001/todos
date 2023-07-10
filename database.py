e3a9c3fe0780c05eab23e45a7eb8e54af54dbbcb
kyongha <magpia001@gmail.com>
2023-07-11 03:58:47 +0900

    simple todo 완성 1차 완성 코드

---
+++ database.py
@@ -0,0 +1,27 @@
+# database.py
+"""
+pip install sqlalchemy
+DB 연결과 관련된 정보 설정
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# DB_URL = 'sqlite:///todo.sqlite3'
+DB_URL = 'sqlite:///todos_db.sqlite3'
+
+# 데이터베이스에 연결하는 엔진을 생성하는 함수
+engine = create_engine(DB_URL, connect_args={'check_same_thread': False})
+
+# 데이터베이스와 상호 작용하는 세션을 생성하는 클래스
+sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# SQLAlchemy의 선언적 모델링을 위한 기본 클래스
+Base = declarative_base()
+
+"""
+declarative_base 클래스는 다음과 같은 기능을 제공함
+- 데이터베이스 모델 클래스를 정의하는 기능
+- 데이터베이스 모델 클래스와 데이터베이스 테이블을 연결하는 기능
+- 데이터베이스 모델 클래스를 사용하여 데이터베이스와 상호 작용하는 기능
+"""
---
+++ main.py
@@ -0,0 +1,85 @@
+""" 
+simple todo
+fastAPI를 활용한 DB CRUD
+명령 실행 : 
+  - app 폴더로 이동
+  - uvicorn main:app --reload
+"""
+# main.py
+
+from fastapi import FastAPI, Request, Depends, Form, status
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+
+from database import engine, sessionlocal
+from sqlalchemy.orm import Session
+import models
+
+# models에 정의한 모든 클래스, 연결한 DB엔진에 테이블로 생성
+models.Base.metadata.create_all(bind=engine)
+
+# FastAPI() 객체 생성
+app = FastAPI()
+
+# html 템플릿 폴더를 지정하여 jinja템플릿 객체 생성
+templates = Jinja2Templates(directory="templates")
+
+# static 폴더(정적파일 폴더)를 app에 연결
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def get_db():
+    db = sessionlocal()
+    try:
+        # yield 키워드는 FastAPI가 함수의 실행을 일시 중지하고 데이터베이스 세션을 호출자에게 반환하도록 지시
+        yield db
+    finally:
+        # 마지막에 무조건 닫음
+        db.close()
+
+@app.get("/")
+async def home(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(models.Todo).order_by(models.Todo.id.desc())
+    # print(todos.count())
+    if todos.count() == 0:
+        todos = 0
+
+    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+    
+
+@app.post("/add")
+async def add(request: Request, task: str = Form(...), db: Session = Depends(get_db)):
+    todo = models.Todo(task=task)
+    db.add(todo)
+    db.commit()
+    # app.url_path_for("home") <= home는 위에 정의한 path /의 함수명
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+# 수정할 todo 클릭했을 때
+@app.get("/edit/{todo_id}")
+async def edit(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    edit_todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todos = db.query(models.Todo).order_by(models.Todo.id.desc())
+    return templates.TemplateResponse("edit.html", {
+        "request": request, 
+        "todo": edit_todo,
+        "todos": todos
+         })
+
+# 값을 수정하고 db 반영 요청
+@app.post("/edit/{todo_id}")
+async def edit(request: Request, todo_id: int, task: str = Form(...), completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.task = task
+    todo.completed = completed
+    db.commit()
+
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/delete/{todo_id}")
+async def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
